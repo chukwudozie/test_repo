005d6f103f6789b666440a27a71f0327c174cf9e
chukwudozie <emkychuks062@gmail.com>
2022-09-19 11:20:26 +0100

    delete commented codes

--- src/main/java/org/design/bonpellz/config/CorsConfig.java
+++ src/main/java/org/design/bonpellz/config/CorsConfig.java
@@ -5,22 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class CorsConfig {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("http://localhost:3000/")
-//                        .allowedOrigins("*")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
-//            }
-//        };
-//    }
-//
-//}
 
 @Configuration
 public class CorsConfig {
@@ -37,16 +21,3 @@ public class CorsConfig {
     }
 }
 
-//
-//@Configuration
-//public class WebMVCConfig implements WebMvcConfigurer {
-//    public void addCorsMapping(CorsRegistry corsRegistry) {
-//        final long MAX_AGE = 3000;
-//        String allowedOrigin = "localhost:3000";
-//        corsRegistry.addMapping("/**")
-//                .allowedOrigins(allowedOrigin)
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                .allowedHeaders("*")
-//                .maxAge(MAX_AGE);
-//    }
-//}
